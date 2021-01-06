e98016953875d04fe5790d267ace32eac6429577
sariaysegul <sariaysegulsari@gmail.com>
2021-01-06 23:03:10 +0300

    tweet count is added

--- app/reports/views.py
+++ app/reports/views.py
@@ -51,22 +51,23 @@ def create_report(request):
         return render(request, 'reports/report_form.html')
 
 
-def report_collect_tweet(request):
-    return render(request, 'reports/report_collect_tweet.html')
-
-
 @csrf_exempt
 def collect_tweets(request):
     keyword = request.POST.get('keyword')
     start_date = request.POST.get('start')
     end_date = request.POST.get('end')
+    count = request.POST.get('count')
     api = twitter.Api(consumer_key=CONSUMER_KEY,
                       consumer_secret=CONSUMER_SECRET,
                       access_token_key=ACCESS_TOKEN,
                       access_token_secret=ACCESS_TOKEN_SECRET)
-    data = api.GetSearch(term=keyword, count=100, lang='en', since=start_date, until=end_date, include_entities=False,
+    data = api.GetSearch(term=keyword, count=count, lang='en', since=start_date, until=end_date, include_entities=False,
                          result_type='recent')
     tweets_text = [[tw.id, tw.created_at, tw.text] for tw in data]
     tweets_text = {'data': tweets_text}
 
     return JsonResponse(tweets_text, safe=False)
+
+
+def report_collect_tweet(request):
+    return render(request, 'reports/report_collect_tweet.html')
