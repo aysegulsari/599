57d23267116152d78e31aef78fc39361c3b720d5
sariaysegul <sariaysegulsari@gmail.com>
2021-01-08 00:31:54 +0300

    collect tweet function is updated

--- app/reports/views.py
+++ app/reports/views.py
@@ -1,15 +1,18 @@
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
 )
-
+import pandas as pd
 from django.shortcuts import render
 from django.views import generic
 from django.views.decorators.csrf import csrf_exempt
 import os
-from . import models
+from . import models as myModels
 from django.http import JsonResponse
 import twitter
 from dotenv import load_dotenv
+import tweepy
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 
 load_dotenv()
 
@@ -21,29 +24,32 @@ ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
 
 class CreateReport(LoginRequiredMixin, generic.CreateView):
     fields = ("name", "description")
-    model = models.Report
+    model = myModels.Report
 
 
 class SingleReport(generic.DetailView):
-    model = models.Report
+    model = myModels.Report
 
 
 class ListReports(generic.ListView):
-    model = models.Report
+    model = myModels.Report
+
+    def get_queryset(self):
+        return myModels.Report.objects.filter(user=self.request.user)
 
 
 def create_report(request):
-    reports = models.Report.objects.filter(user=request.user)
+    reports = myModels.Report.objects.filter(user=request.user)
     if request.method == 'POST':
         user = request.user
         name = request.POST.get('name')
         description = request.POST.get('description')
         keyword = request.POST.get('keyword')
         if name is not None:
-            reports = models.Report.objects.filter(name=name, user=user)
+            reports = myModels.Report.objects.filter(name=name, user=user)
             if not reports:
-                models.Report.objects.create(name=name, description=description, keyword=keyword, user=user)
-            reports = models.Report.objects.filter(user=request.user)
+                myModels.Report.objects.create(name=name, description=description, keyword=keyword, user=user)
+            reports = myModels.Report.objects.filter(user=request.user)
         return render(request, 'reports/report_list.html',
                       {'object_list': reports,
                        })
@@ -54,18 +60,36 @@ def create_report(request):
 @csrf_exempt
 def collect_tweets(request):
     keyword = request.POST.get('keyword')
+    count = request.POST.get('count')
+    if not count:
+        count="100"
     start_date = request.POST.get('start')
     end_date = request.POST.get('end')
-    count = request.POST.get('count')
-    api = twitter.Api(consumer_key=CONSUMER_KEY,
-                      consumer_secret=CONSUMER_SECRET,
-                      access_token_key=ACCESS_TOKEN,
-                      access_token_secret=ACCESS_TOKEN_SECRET)
-    data = api.GetSearch(term=keyword, count=count, lang='en', since=start_date, until=end_date, include_entities=False,
+    print(count)
+    limit = int(count)
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
+    '''
+    api = twitter.Api(consumer_key=CONSUMER_KEY,consumer_secret=CONSUMER_SECRET,
+            access_token_key=ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET)
+    data = api.GetSearch(term=keyword,count=100, lang='en', since=start_date, until=end_date, include_entities=False,
                          result_type='recent')
-    tweets_text = [[tw.id, tw.created_at, tw.text] for tw in data]
-    tweets_text = {'data': tweets_text}
+    '''
+    i = 0
+    data = []
+    for tweet in tweepy.Cursor(api.search, q=keyword, count=count, lang='tr', tweet_mode='extended', since=start_date,
+                               until=end_date).items():
+        data.append(tweet)
+        i += 1
+        if i >= limit:
+            break
+        else:
+            pass
 
+    tweets_text = [[1, str(tw.created_at), tw.full_text] for tw in data]
+    tweets_text = {'data': tweets_text}
+    print(tweets_text)
     return JsonResponse(tweets_text, safe=False)
 
 
