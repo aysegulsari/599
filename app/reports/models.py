01206b7f38c55010ef2067b746c37888b02854c0
sariaysegul <sariaysegulsari@gmail.com>
2021-01-16 20:46:06 +0300

    report model fields are updated

--- app/reports/models.py
+++ app/reports/models.py
@@ -5,13 +5,11 @@ from django.utils.text import slugify
 # from accounts.models import User
 
 from django.contrib.auth import get_user_model
-
 User = get_user_model()
 
 # https://docs.djangoproject.com/en/2.0/howto/custom-template-tags/#inclusion-tags
 # This is for the in_group_members check template tag
 from django import template
-
 register = template.Library()
 
 
@@ -19,7 +17,8 @@ class Report(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(allow_unicode=True, unique=True)
-    description = models.TextField(blank=False, default='')
+    time_interval = models.TextField(blank=False, default='')
+    tweet_count= models.TextField(blank=False, default='')
     keyword = models.TextField(blank=False, default='')
 
     def __str__(self):
