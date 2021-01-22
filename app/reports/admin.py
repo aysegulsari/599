793bd39b5e4290c5202db6ad662b48d44c228352
sariaysegul <sariaysegulsari@gmail.com>
2021-01-23 01:05:38 +0300

    hashtag and context annotation models are added

--- app/reports/admin.py
+++ app/reports/admin.py
@@ -4,3 +4,5 @@ from . import models
 # Register your models here.
 admin.site.register(models.Report)
 admin.site.register(models.Tweet)
+admin.site.register(models.Hashtag)
+admin.site.register(models.ContextAnnotation)
--- app/reports/models.py
+++ app/reports/models.py
@@ -5,11 +5,13 @@ from django.utils.text import slugify
 # from accounts.models import User
 
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
 
 # https://docs.djangoproject.com/en/2.0/howto/custom-template-tags/#inclusion-tags
 # This is for the in_group_members check template tag
 from django import template
+
 register = template.Library()
 
 
@@ -17,10 +19,10 @@ class Report(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(allow_unicode=True, unique=True)
-    time_interval = models.TextField(blank=False, default='')
-    tweet_count = models.TextField(blank=False, default='')
-    keyword = models.TextField(blank=False, default='')
-    language = models.TextField(blank=False, default='')
+    time_interval = models.CharField(blank=False, max_length=50, default='')
+    tweet_count = models.CharField(blank=False, max_length=10, default='')
+    keyword = models.CharField(blank=False, max_length=50, default='')
+    language = models.CharField(blank=False, max_length=10, default='')
 
     def __str__(self):
         return self.name
@@ -37,14 +39,32 @@ class Report(models.Model):
 
 
 class Tweet(models.Model):
-    report = models.ForeignKey(Report, on_delete=models.CASCADE)
+    reports = models.ManyToManyField(Report)
     tweet_id = models.CharField(blank=False, max_length=100, default='')
     creation_date = models.CharField(blank=False, max_length=100, default='')
-    tweet_context=models.CharField(blank=False, max_length=100, default='')
     tweet_text = models.TextField(blank=False, default='')
-    category = models.CharField(blank=False, max_length=100, default='')
-    negative= models.CharField(blank=False, max_length=10, default='')
-    positive= models.CharField(blank=False, max_length=10, default='')
-    neutral= models.CharField(blank=False, max_length=10, default='')
-    compound=models.CharField(blank=False, max_length=100, default='')
-    sentiment=models.CharField(blank=False, max_length=10, default='')
+    sentiment = models.CharField(blank=False, max_length=10, default='')
+    lang = models.CharField(blank=False, max_length=10, default='')
+    retweet_count = models.CharField(blank=False, max_length=10, default='')
+    reply_count = models.CharField(blank=False, max_length=10, default='')
+    like_count = models.CharField(blank=False, max_length=10, default='')
+    # category = models.CharField(blank=False, max_length=100, default='')
+    # negative= models.CharField(blank=False, max_length=10, default='')
+    # positive= models.CharField(blank=False, max_length=10, default='')
+    # neutral= models.CharField(blank=False, max_length=10, default='')
+    # compound=models.CharField(blank=False, max_length=100, default='')
+    # tweet_context=models.CharField(blank=False, max_length=100, default='')
+
+
+class Hashtag(models.Model):
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    tag = models.TextField(blank=False, default='')
+
+
+class ContextAnnotation(models.Model):
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    domain_id = models.CharField(blank=False, max_length=100, default='')
+    domain_name = models.CharField(blank=False, max_length=100, default='')
+    domain_desc = models.CharField(blank=False, max_length=250, default='')
+    entity_id = models.CharField(blank=False, max_length=100, default='')
+    entity_name = models.CharField(blank=False, max_length=100, default='')
